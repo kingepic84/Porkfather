f7209671e53d5be0869df1cfd102c6a9e3570909
kingepic84 <kingepic84@aol.com>
2024-08-11 01:03:39 -0500

    qefgbaergq

--- Porkbot.py
+++ Porkbot.py
@@ -501,11 +501,13 @@ async def player(inter: Interaction):
 @tree.command(name="mediaplayer", description="Alias for /videoplayer")
 async def mplay(inter: Interaction):
     await inter.response.defer(thinking=True)
+    print("here player")
     await player(inter)
     
 @tree.command(name="videoplayer", description="Start the videoplayer")
 async def vplayer(inter: Interaction):
     await inter.response.defer(thinking=True)
+    print("here player")
     await player(inter)
 
 @tree.command(name="refresh", description="Resends the videoplayer embed")
