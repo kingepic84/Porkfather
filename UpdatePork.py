971e14546c4b15e5a3049a1b53f1265bfb65da07
kingepic84 <kingepic84@aol.com>
2024-09-06 17:19:39 -0400

    Added UpdatePork file

---
+++ UpdatePork.py
@@ -0,0 +1,9 @@
+import psutil,subprocess, sys
+for process in psutil.process_iter():
+    if process.name() == "python3 Porkbot.py" or process.name() == "py Porkbot.py":
+        process.kill()
+        process.wait()
+git = subprocess.run("git pull", stdout=sys.stdout, stderr=sys.stderr)
+if git.check_returncode():
+    runpy = subprocess.Popen("nohup py Porkbot.py", stdout=sys.stdout, stderr=sys.stderr)
+print('Porkfather Restarted')
