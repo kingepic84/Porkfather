9ed0b8008bf87676a9fd28f6da046be0b40700be
kingepic84 <kingepic84@aol.com>
2024-09-06 17:45:45 -0400

    wrgqeh

--- UpdatePork.py
+++ UpdatePork.py
@@ -1,9 +1,10 @@
-import psutil,subprocess, sys
+import psutil,subprocess,sys
+from subprocess import DEVNULL
 for process in psutil.process_iter():
     if "python3 Porkbot.py" in process.name():
         process.kill()
         process.wait()
 git = subprocess.run(["git", "pull"], stdout=sys.stdout, stderr=sys.stderr)
-pork = subprocess.Popen(["nohup python3 Porkbot.py"], stdout="/dev/null", stderr=sys.stderr, shell=True)
+pork = subprocess.Popen(["nohup python3 Porkbot.py"], stdout=DEVNULL, stderr=sys.stdout, shell=True)
 print("Porkfather Restarted")
 
