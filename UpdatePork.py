fad290ef8fb0694abb337804a0fe1697bf5db934
kingepic84 <kingepic84@aol.com>
2024-09-06 17:50:28 -0400

    qetnwrm

--- UpdatePork.py
+++ UpdatePork.py
@@ -2,7 +2,7 @@ import psutil,subprocess,sys
 from subprocess import DEVNULL
 for process in psutil.process_iter():
     if "Porkbot.py" in process.name():
-        process.kill()
+        process.terminate()
         process.wait()
 git = subprocess.run(["git", "pull"], stdout=sys.stdout, stderr=sys.stderr)
 pork = subprocess.Popen(["nohup python3 Porkbot.py"], stdout=sys.stdout, stderr=sys.stderr, shell=True)
