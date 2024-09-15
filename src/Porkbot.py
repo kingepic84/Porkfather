5a1651cebec39f752c4a33041a51ef0070a7a8bb
kingepic84 <kingepic84@aol.com>
2024-09-15 03:37:44 -0400

    wrthj24j2

--- src/Porkbot.py
+++ src/Porkbot.py
@@ -696,7 +696,7 @@ async def throw(inter: Interaction):
 async def playFile(inter: Interaction, file: Attachment):
     if inter.guild.id == 727745299614793728:
         if inter.guild.voice_client is not None:
-            await inter.response.send_message("You cant use this command when I'm already in a VC!")
+            await inter.response.send_message("You cant use this command when I'm already in a VC!", delete_after=5)
         else:
             channel = await inter.user.voice.channel.connect()
             coro = disconnect(channel, inter)
