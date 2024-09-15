2e39c0e1093397e2ec971651aff8ee30e4f450d4
kingepic84 <kingepic84@aol.com>
2024-09-15 01:05:39 -0400

    aerhwtkw5y

--- Porkbot.py
+++ Porkbot.py
@@ -665,6 +665,18 @@ async def nugget(inter: Interaction):
     else:
         await inter.response.send_message(content="# YOU CANT USE THIS COMMAND IN THIS SERVER!\nhttps://tenor.com/view/wheeze-laugh-gif-14359545", delete_after=5)
 
+@tree.command(name="gedagedigedagedo_anime", description="Funny Chicken Nugget")
+async def nugget(inter: Interaction):
+    if inter.guild.id == 727745299614793728:
+        if inter.guild.voice_client is not None:
+            await inter.response.send_message("I'm already in a VC!")
+        channel = await inter.user.voice.channel.connect()
+        coro = disconnect(channel)
+        channel.play(FFmpegPCMAudio("yt1s.com_-_Gedagedigedagedo.mp4"), after=lambda e: asyncio.run_coroutine_threadsafe(coro, client.loop))
+        await inter.response.send_message("Gedagedigedagedo".upper(), delete_after=5)
+    else:
+        await inter.response.send_message(content="# YOU CANT USE THIS COMMAND IN THIS SERVER!\nhttps://tenor.com/view/wheeze-laugh-gif-14359545", delete_after=5)
+
 
 @tree.command(name="rock", description="Throws a Rock")
 async def throw(inter: Interaction):
